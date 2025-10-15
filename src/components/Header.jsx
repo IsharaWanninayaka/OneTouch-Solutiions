2da755efed220d7da63b5a5185dfb2c2227f539f
Ishara Wanninayaka <165476861+IsharaWanninayaka@users.noreply.github.com>
2025-10-15 20:17:22 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ function Header() {
             <div className="flex items-center">
               <img
                 onClick={() => navigate("/")}
-                src="/logo3.png"
+                src="/images/Logo3.png"
                 alt="Logo"
                 width={150}
                 height={80}
